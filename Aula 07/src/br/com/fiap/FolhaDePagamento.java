b8cd88f5d89490b516ce69f4739c74d940145d9b
Felipe Balbino Murad <felipebmurad@gmail.com>
2026-03-09 09:12:48 -0300

    Add files via upload

---
+++ Aula 07/src/br/com/fiap/FolhaDePagamento.java
@@ -0,0 +1,17 @@
+package br.com.fiap;
+
+public class FolhaDePagamento {
+    //atributos
+    public double salarioBruto;
+    public int numeroDeDependentes;
+    public double descontoINSS;
+    public double valorPlanoDeSaude;
+    //métodos da classe
+    public double calcularSalarioLiquido(){
+        double desconto1, desconto2, salarioLiquido;
+        desconto1 = salarioBruto * (descontoINSS / 100);
+        desconto2 = valorPlanoDeSaude * (numeroDeDependentes + 1);
+        salarioLiquido = salarioBruto - (desconto1 + desconto2);
+        return salarioLiquido;
+    }
+}
